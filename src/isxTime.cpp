b8fa5058b154dacadc1c8bff018510970b44b0fb
Andy Sweet <andy@inscopix.com>
2015-08-06 12:43:01 -0700

    Fix Linux include for exceptions

--- src/isxTime.cpp
+++ src/isxTime.cpp
@@ -1,6 +1,7 @@
 #include <math.h>
 #include "isxTime.h"
 #include <QTimeZone>
+#include <stdexcept>
 
 namespace isx {
 
@@ -14,26 +15,26 @@ Time::Time() {
 
 Time::Time(uint16_t year, uint8_t mon, uint8_t day, uint8_t hour, uint8_t mins, uint8_t secs, double mSecs, int8_t utcOff) {
 	if (year < 1970)
-		throw std::domain_error("Year must be in [1970, 2^16).");
+		throw std::runtime_error("Year must be in [1970, 2^16).");
 	if (mon < 1 || mon > 12)
-		throw std::domain_error("Month must be in [1, 12].");
+		throw std::runtime_error("Month must be in [1, 12].");
 	if (day < 1 || day > 31)
-		throw std::domain_error("Day must be in [1, 31].");
+		throw std::runtime_error("Day must be in [1, 31].");
 	if (hour > 23)
-		throw std::domain_error("Hour must be in [0, 23].");
+		throw std::runtime_error("Hour must be in [0, 23].");
 	if (mins > 59)
-		throw std::domain_error("Minutes must be in [0, 59].");
+		throw std::runtime_error("Minutes must be in [0, 59].");
 	if (secs > 59)
-		throw std::domain_error("Seconds must be in [0, 59].");
+		throw std::runtime_error("Seconds must be in [0, 59].");
 	if (mSecs < 0 || mSecs >= 1000)
-		throw std::domain_error("Milliseconds must be in [0, 1000).");
+		throw std::runtime_error("Milliseconds must be in [0, 1000).");
 	if (utcOff < -14 || utcOff > 14)
-		throw std::domain_error("UTC offset must be in [-14, 14].");
+		throw std::runtime_error("UTC offset must be in [-14, 14].");
 
 	QDate date(year, mon, day);
 
 	if (!date.isValid())
-		throw std::invalid_argument("Date " + date.toString().toStdString() + " is not valid.");
+		throw std::runtime_error("Date " + date.toString().toStdString() + " is not valid.");
 
 	QTime time(hour, mins, secs);
 	QTimeZone zone(utcOff);
