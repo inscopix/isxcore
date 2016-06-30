3e992e947095873ddeb03c47a7fc184ff43472d6
Sylvana Alpert <salpert@inscopix.com>
2016-06-30 15:30:25 -0700

    Fixed uncaught exception

--- src/isxHdf5File.cpp
+++ src/isxHdf5File.cpp
@@ -1,4 +1,6 @@
 #include "isxHdf5File.h"
+//#include "isxLog.h"
+#include "isxException.h"
 #include "H5Cpp.h"
 
 namespace isx
@@ -6,8 +8,20 @@ namespace isx
 void
 Hdf5File::getObjNames(const std::string & inFileName, const std::string & inPath, std::vector<std::string> & outNames)
 {
-    H5::H5File file(inFileName.c_str(), H5F_ACC_RDONLY);
-    H5::Group rootGroup = file.openGroup(inPath);
+    H5::Group rootGroup;
+
+    try
+    {
+        H5::H5File file(inFileName.c_str(), H5F_ACC_RDONLY);
+        rootGroup = file.openGroup(inPath);
+    }
+    catch (const H5::FileIException& error)
+    {
+        ISX_THROW(isx::ExceptionFileIO,
+            "Failure caused by H5File operations.\n", error.getDetailMsg());
+    }
+
+
     hsize_t nObjInGroup = rootGroup.getNumObjs();
 
     if (0 == nObjInGroup)
