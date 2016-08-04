96c612589e896aabee4375ce6e0d731bdd718028
Axel Schildan <aschildan@inscopix.com>
2016-08-04 11:20:37 -0700

    Add unit test for IoTask cancellation.

--- test/IoQueueTest.cpp
+++ test/IoQueueTest.cpp
@@ -18,7 +18,7 @@ TEST_CASE("IoQueue", "[core]") {
 
     SECTION("dispatch task to IoQueue") {
         bool taskRan = false;
-        isx::AsyncTaskStatus status = isx::AsyncTaskStatus::UNKNOWN_ERROR;
+        isx::AsyncTaskStatus status = isx::AsyncTaskStatus::PENDING;
         auto ioTask = std::make_shared<isx::IoTask>(
             [&]()
             {
@@ -41,5 +41,47 @@ TEST_CASE("IoQueue", "[core]") {
         REQUIRE(status == isx::AsyncTaskStatus::COMPLETE);
     }
 
+    SECTION("IoTask cancellation") {
+        bool slowTaskRan = false;
+        bool ioTaskRan = false;
+        isx::AsyncTaskStatus slowTaskStatus = isx::AsyncTaskStatus::PENDING;
+        isx::AsyncTaskStatus ioTaskStatus = isx::AsyncTaskStatus::PENDING;
+        
+        auto slowTask = std::make_shared<isx::IoTask>(
+            [&]()
+            {
+                std::chrono::milliseconds d(2);
+                std::this_thread::sleep_for(d);
+                slowTaskRan = true;
+            },
+            [&](isx::AsyncTaskStatus inStatus){
+                slowTaskStatus = inStatus;
+            });
+        auto ioTask = std::make_shared<isx::IoTask>(
+            [&]()
+            {
+                ioTaskRan = true;
+            },
+            [&](isx::AsyncTaskStatus inStatus){
+                ioTaskStatus = inStatus;
+            });
+        slowTask->schedule();
+        ioTask->schedule();
+        ioTask->cancel();
+        for (int i = 0; i < 250; ++i)
+        {
+            if (slowTaskRan)
+            {
+                break;
+            }
+            std::chrono::milliseconds d(2);
+            std::this_thread::sleep_for(d);
+        }
+        REQUIRE(slowTaskRan);
+        REQUIRE(slowTaskStatus == isx::AsyncTaskStatus::COMPLETE);
+        REQUIRE(!ioTaskRan);
+        REQUIRE(ioTaskStatus == isx::AsyncTaskStatus::CANCELLED);
+    }
+    
     isx::CoreShutdown();
 }
