cac159b807df433aece062d0ea16caa48b847ec0
dkoppel-inscopix <dkoppel@inscopix.com>
2017-11-02 13:27:02 -0700

    removed commented out line

--- src/isxMovieCompressedAviExporter.cpp
+++ src/isxMovieCompressedAviExporter.cpp
@@ -39,7 +39,6 @@ runMovieCompressedAviExporter(MovieCompressedAviExporterParams inParams, std::sh
     {
         try
         {
-            //inParams.m_compressedAviFilename = "C:\\Users\\dkoppel\\compressedMovies\\sample.avi"; // REMOVE THIS!
             cancelled = toCompressedAVI(inParams.m_compressedAviFilename, inParams.m_srcs, inParams.m_numFramesInMovie, inCheckInCB);
         }
         catch (...)
