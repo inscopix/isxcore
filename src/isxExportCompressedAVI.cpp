c5982b26b577d8986bd732c23b9244ce511b84f6
dkoppel-inscopix <dkoppel@inscopix.com>
2017-11-09 20:27:31 -0800

    yes

--- src/isxExportCompressedAVI.cpp
+++ src/isxExportCompressedAVI.cpp
@@ -28,7 +28,7 @@ compressedAVIFindMinMax(const std::string & inFileName, const std::vector<SpMovi
             {
                 auto f = m->getFrame(i);
                 auto& img = f->getImage();
-                int numPixels = int(img.getWidth() * img.getHeight());
+                //int numPixels = int(img.getWidth() * img.getHeight());
 
 				float minValLocal = -1;
 				float maxValLocal = -1;
