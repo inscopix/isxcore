3ffc6fc7bdbf2fc0a307140c753486d0e32b6c71
Andy Sweet <andy@inscopix.com>
2018-11-08 16:22:30 -0800

    MOS-1727 working and fast on macOS

--- src/isxExportTiff.h
+++ src/isxExportTiff.h
@@ -18,27 +18,29 @@ class TiffExporter
 {
 public:
     /// constructor
-    /// 
+    ///
     /// \param inFileName   out file path
     /// \param inBigTiff    if true, write to the BigTIFF format, otherwise don't
     TiffExporter(const std::string & inFileName, const bool inBigTiff = false);
 
     /// destructor
-    /// 
+    ///
     ~TiffExporter();
 
     /// writes image to current Tiff directory
-    /// 
+    ///
     /// \param inImage       pointer to Image
     /// \param inZeroImage   boolean flag to write black (zero) TIFF using inImage parameters (e.g. width, height, number of channels etc.)
     void toTiffOut(const Image * inImage, const bool inZeroImage = false);
 
     /// switch to next TIFF Directory/Frame
-    /// 
+    ///
     void nextTiffDir();
 private:
+    /// The opaque TIFF directory struct used by libtiff.
     TIFF * out;
-    //int fd;
+    /// The offset of the last TIFF directory written.
+    unsigned long lastOffDir = 0;
 };
 
 } // namespace isx
