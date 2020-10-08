c6e76c927c72d0d5b657e99c5d09a67d45e58680
brunoboivin <bruno.boivin@mail.mcgill.ca>
2020-10-08 16:30:48 -0700

    centralized pre-mc metadata

--- api/isxMetadata.h
+++ api/isxMetadata.h
@@ -29,12 +29,15 @@ namespace isx
     };
     /// \endcond doxygen chokes on enum class inside of namespace
 
+    /// Struct for cell-set-specific metadata
     struct CellSetMetadata
     {
+        /// empty constructor
         CellSetMetadata()
         {
         }
 
+        /// fully specified constructor
         CellSetMetadata(
             const CellSetMethod_t method,
             const CellSetType_t type)
@@ -43,11 +46,50 @@ namespace isx
         {
         }
 
-        CellSetMethod_t  m_method = CellSetMethod_t::UNAVAILABLE;
-        CellSetType_t  m_type = CellSetType_t::UNAVAILABLE;
+        CellSetMethod_t  m_method = CellSetMethod_t::UNAVAILABLE;  ///< method used to generate the cell set
+        CellSetType_t  m_type = CellSetType_t::UNAVAILABLE;        ///< type of footprints in the cell set
     };
 
+    /// Struct for holding pre-motion-correction metadata
+    struct PreMotionCorrMetadata
+    {
+        /// empty constructor
+        PreMotionCorrMetadata()
+        {
+        }
 
+        /// fully specified constructor
+        PreMotionCorrMetadata(
+            const size_t topLeftX,
+            const size_t topLeftY,
+            const size_t width,
+            const size_t height)
+            : m_topLeftX(topLeftX)
+            , m_topLeftY(topLeftY)
+            , m_width(width)
+            , m_height(height)
+        {
+        }
+
+        size_t m_topLeftX = 0;   ///< top left corner x coordinate
+        size_t m_topLeftY = 0;   ///< top left corner y coordinate
+        size_t m_width;          ///< width of the data prior to motion correction
+        size_t m_height;         ///< height of the data prior to motion correction
+    };
+
+    template <typename T>
+    nlohmann::json getExtraPropertiesJSON(T & inData)
+    {
+        using json = nlohmann::json;
+        json extraProps = json::parse(inData->getExtraProperties());
+        if (!extraProps["idps"].is_null() && !extraProps["idps"].is_object() && extraProps["idps"].is_string())
+        {
+            extraProps["idps"] = json::object();
+        }
+        return extraProps;
+    }
+
+    // Getters
     inline std::string getCellSetMethodString(CellSetMethod_t cellSetMethod)
     {
         switch(cellSetMethod)
@@ -78,14 +120,12 @@ namespace isx
         }
     }
 
-
-    // Getters
     template <class T>
     CellSetMethod_t getCellSetMethod(T & inData)
     {
         using json = nlohmann::json;
-        json extraProps = json::parse(inData->getExtraProperties());
-        if (extraProps["idps"] != "" && !extraProps["idps"]["cellset"]["method"].is_null())
+        json extraProps = getExtraPropertiesJSON(inData);
+        if (!extraProps["idps"]["cellset"]["method"].is_null())
         {
             std::string method = extraProps["idps"]["cellset"]["method"].get<std::string>();
             if (method == "pca-ica") return CellSetMethod_t::PCAICA;
@@ -110,16 +150,20 @@ namespace isx
         return CellSetType_t::UNAVAILABLE;
     }
 
-    template <typename T>
-    nlohmann::json getExtraPropertiesJSON(T & inData)
+    template <class T>
+    PreMotionCorrMetadata getPreMotionCorrMetadata(T & inData)
     {
+        PreMotionCorrMetadata preMotionCorrMetadata;
         using json = nlohmann::json;
-        json extraProps = json::parse(inData->getExtraProperties());
-        if (!extraProps["idps"].is_null() && !extraProps["idps"].is_object() && extraProps["idps"].is_string())
+        json extraProps = getExtraPropertiesJSON(inData);
+        if (!extraProps["idps"]["pre_mc"].is_null())
         {
-            extraProps["idps"] = json::object();
+            preMotionCorrMetadata.m_topLeftX = extraProps["idps"]["pre_mc"]["topLeft"]["x"].get<size_t>();
+            preMotionCorrMetadata.m_topLeftY = extraProps["idps"]["pre_mc"]["topLeft"]["y"].get<size_t>();
+            preMotionCorrMetadata.m_width = extraProps["idps"]["pre_mc"]["width"].get<size_t>();
+            preMotionCorrMetadata.m_height = extraProps["idps"]["pre_mc"]["height"].get<size_t>();
         }
-        return extraProps;
+        return preMotionCorrMetadata;
     }
 
     // Setters
@@ -151,6 +195,19 @@ namespace isx
     }
 
     template <typename T>
+    void setPreMotionCorrMetadata(T & inData, PreMotionCorrMetadata preMotionCorrMetadata)
+    {
+        using json = nlohmann::json;
+        json extraProps = getExtraPropertiesJSON(inData);
+        extraProps["idps"]["pre_mc"]["topLeft"]["x"] = preMotionCorrMetadata.m_topLeftX;
+        extraProps["idps"]["pre_mc"]["topLeft"]["y"] = preMotionCorrMetadata.m_topLeftY;
+        extraProps["idps"]["pre_mc"]["width"] = preMotionCorrMetadata.m_width;
+        extraProps["idps"]["pre_mc"]["height"] = preMotionCorrMetadata.m_height;
+        inData->setExtraProperties(extraProps.dump());
+    }
+
+    // Helper function to deal with string representations of the extra properties
+    template <typename T>
     std::string addCellSetMetadata(T & inData, CellSetMetadata cellSetMetadata)
     {
         using json = nlohmann::json;
@@ -169,6 +226,28 @@ namespace isx
         return extraProps.dump();
     }
 
+    template <typename Src, typename Dst>
+    void transferPreMotionCorrMetadata(Src & src, Dst & dst)
+    {
+        using json = nlohmann::json;
+        json extraProps = getExtraPropertiesJSON(src);
+        dst->setExtraProperties(extraProps.dump());
+
+        PreMotionCorrMetadata preMotionCorrMetadata(
+            static_cast<size_t>(src->getSpacingInfo().getTopLeft().getX().toDouble()),
+            static_cast<size_t>(src->getSpacingInfo().getTopLeft().getY().toDouble()),
+            src->getSpacingInfo().getNumPixels().getWidth(),
+            src->getSpacingInfo().getNumPixels().getHeight());
+        setPreMotionCorrMetadata(dst, preMotionCorrMetadata);
+    }
+
+    template <typename T>
+    bool hasPreMotionCorrMetadata(const T & inData)
+    {
+        using json = nlohmann::json;
+        json extraProps = getExtraPropertiesJSON(inData);
+        return !extraProps["idps"]["pre_mc"].is_null();
+    }
 
 } // namespace isx
 
