9577abefec8b0c60a1b3cc8fd90a3c4130f52ebf
brunoboivin <bruno.boivin@mail.mcgill.ca>
2021-02-16 16:42:55 -0800

    IDPS-372 simple vessel set

---
+++ src/isxVesselSetSimple.cpp
@@ -0,0 +1,371 @@
+#include "isxVesselSetSimple.h"
+#include "isxVesselSetFile.h"
+#include "isxConditionVariable.h"
+#include "isxIoTask.h"
+#include "isxIoTaskTracker.h"
+
+namespace isx
+{
+
+VesselSetSimple::VesselSetSimple()
+{
+}
+
+VesselSetSimple::VesselSetSimple(const std::string & inFileName, bool enableWrite)
+    : m_valid(false)
+    , m_traceIoTaskTracker(new IoTaskTracker<FTrace_t>())
+    , m_imageIoTaskTracker(new IoTaskTracker<Image>())
+{
+    m_file = std::make_shared<VesselSetFile>(inFileName, enableWrite);
+    m_valid = true;
+}
+
+VesselSetSimple::VesselSetSimple(
+        const std::string & inFileName,
+        const TimingInfo & inTimingInfo,
+        const SpacingInfo & inSpacingInfo,
+        const bool inIsRoiSet)
+    : m_valid(false)
+    , m_traceIoTaskTracker(new IoTaskTracker<FTrace_t>())
+    , m_imageIoTaskTracker(new IoTaskTracker<Image>())
+{
+    m_file = std::make_shared<VesselSetFile>(inFileName, inTimingInfo, inSpacingInfo, inIsRoiSet);
+    m_valid = true;
+}
+
+VesselSetSimple::~VesselSetSimple()
+{
+}
+
+bool
+VesselSetSimple::isValid() const
+{
+    return m_valid;
+}
+
+void
+VesselSetSimple::closeForWriting()
+{
+    m_file->closeForWriting();
+}
+
+std::string
+VesselSetSimple::getFileName() const
+{
+    return m_file->getFileName();
+}
+
+const isize_t
+VesselSetSimple::getNumVessels()
+{
+    return m_file->numberOfVessels();
+}
+
+isx::TimingInfo
+VesselSetSimple::getTimingInfo() const
+{
+    return m_file->getTimingInfo();
+}
+
+isx::TimingInfos_t
+VesselSetSimple::getTimingInfosForSeries() const
+{
+    TimingInfos_t tis = TimingInfos_t{m_file->getTimingInfo()};
+    return tis;
+}
+
+isx::SpacingInfo
+VesselSetSimple::getSpacingInfo() const
+{
+    return m_file->getSpacingInfo();
+}
+
+SpFTrace_t
+VesselSetSimple::getTrace(isize_t inIndex)
+{
+    Mutex mutex;
+    ConditionVariable cv;
+    mutex.lock("getTrace");
+    AsyncTaskResult<SpFTrace_t> asyncTaskResult;
+    getTraceAsync(inIndex, [&asyncTaskResult, &cv, &mutex](AsyncTaskResult<SpFTrace_t> inAsyncTaskResult)
+        {
+            mutex.lock("getTrace async");
+            asyncTaskResult = inAsyncTaskResult;
+            mutex.unlock();
+            cv.notifyOne();
+        }
+    );
+    cv.wait(mutex);
+    mutex.unlock();
+
+    return asyncTaskResult.get();   // throws is asyncTaskResult contains an exception
+}
+
+void
+VesselSetSimple::getTraceAsync(isize_t inIndex, VesselSetGetTraceCB_t inCallback)
+{
+    // Only get a weak pointer to this, so that we don't bother reading
+    // if this has been deleted when the read gets executed.
+    std::weak_ptr<VesselSetSimple> weakThis = shared_from_this();
+    GetTraceCB_t getTraceCB = [weakThis, this, inIndex]()
+        {
+            auto sharedThis = weakThis.lock();
+            if (sharedThis)
+            {
+                return m_file->readTrace(inIndex);
+            }
+            return SpFTrace_t();
+        };
+    m_traceIoTaskTracker->schedule(getTraceCB, inCallback);
+}
+
+SpImage_t
+VesselSetSimple::getImage(isize_t inIndex)
+{
+    Mutex mutex;
+    ConditionVariable cv;
+    mutex.lock("getImage");
+    AsyncTaskResult<SpImage_t> asyncTaskResult;
+    getImageAsync(inIndex,
+        [&asyncTaskResult, &cv, &mutex](AsyncTaskResult<SpImage_t> inAsyncTaskResult)
+        {
+            mutex.lock("getImage async");
+            asyncTaskResult = inAsyncTaskResult;
+            mutex.unlock();
+            cv.notifyOne();
+        }
+    );
+    cv.wait(mutex);
+    mutex.unlock();
+
+    return asyncTaskResult.get();   // throws if asyncTaskResults contains an exception
+}
+
+void
+VesselSetSimple::getImageAsync(isize_t inIndex, VesselSetGetImageCB_t inCallback)
+{
+    // Only get a weak pointer to this, so that we don't bother reading
+    // if this has been deleted when the read gets executed.
+    std::weak_ptr<VesselSetSimple> weakThis = shared_from_this();
+    GetImageCB_t getImageCB = [weakThis, this, inIndex]()
+        {
+            auto sharedThis = weakThis.lock();
+            SpImage_t im;
+            if (sharedThis)
+            {
+                im = m_file->readSegmentationImage(inIndex);
+            }
+            return im;
+        };
+    m_imageIoTaskTracker->schedule(getImageCB, inCallback);
+}
+
+void
+VesselSetSimple::writeImageAndTrace(
+        isize_t inIndex,
+        const SpImage_t & inImage,
+        SpFTrace_t & inTrace,
+        const std::string & inName)
+{
+    // Get a new shared pointer to the file, so we can guarantee the write.
+    std::shared_ptr<VesselSetFile> file = m_file;
+    Mutex mutex;
+    ConditionVariable cv;
+    mutex.lock("VesselSetSimple::writeImageAndTrace");
+    auto writeIoTask = std::make_shared<IoTask>(
+        [file, inIndex, inImage, inTrace, inName]()
+        {
+            file->writeVesselData(inIndex, *inImage, *inTrace, inName);
+        },
+        [&cv, &mutex](AsyncTaskStatus inStatus)
+        {
+            if (inStatus != AsyncTaskStatus::COMPLETE)
+            {
+                ISX_LOG_ERROR("An error occurred while writing image and trace data to a VesselSet.");
+            }
+            // will only be able to take lock when client reaches cv.wait
+            mutex.lock("VesselSetwriteImageAndTrace finished");
+            mutex.unlock();
+            cv.notifyOne();
+        });
+    writeIoTask->schedule();
+    cv.wait(mutex);
+    mutex.unlock();
+
+    if(writeIoTask->getTaskStatus() == AsyncTaskStatus::ERROR_EXCEPTION)
+    {
+        std::rethrow_exception(writeIoTask->getExceptionPtr());
+    }
+}
+
+VesselSet::VesselStatus
+VesselSetSimple::getVesselStatus(isize_t inIndex)
+{
+    return m_file->getVesselStatus(inIndex);
+}
+
+Color
+VesselSetSimple::getVesselColor(isize_t inIndex)
+{
+    return m_file->getVesselColor(inIndex);
+}
+
+std::string
+VesselSetSimple::getVesselStatusString(isize_t inIndex)
+{
+    return m_file->getVesselStatusString(inIndex);
+}
+
+void
+VesselSetSimple::setVesselStatus(isize_t inIndex, VesselSet::VesselStatus inStatus)
+{
+    m_file->setVesselStatus(inIndex, inStatus);
+}
+
+void
+VesselSetSimple::setVesselColor(isize_t inIndex, const Color& inColor)
+{
+    m_file->setVesselColor(inIndex, inColor);
+}
+
+void
+VesselSetSimple::setVesselColors(const IdColorPairs &inColors)
+{
+    m_file->setVesselColors(inColors);
+}
+
+std::string
+VesselSetSimple::getVesselName(isize_t inIndex)
+{
+    return m_file->getVesselName(inIndex);
+}
+
+void
+VesselSetSimple::setVesselName(isize_t inIndex, const std::string & inName)
+{
+    m_file->setVesselName(inIndex, inName);
+}
+
+std::vector<bool>
+VesselSetSimple::getVesselActivity(isize_t inIndex) const
+{
+    return {m_file->isVesselActive(inIndex)};
+}
+
+void
+VesselSetSimple::setVesselActive(isize_t inIndex, const std::vector<bool> & inActive)
+{
+    ISX_ASSERT(inActive.size() == 1);
+    m_file->setVesselActive(inIndex, inActive.front());
+}
+
+void
+VesselSetSimple::cancelPendingReads()
+{
+    m_imageIoTaskTracker->cancelPendingTasks();
+    m_traceIoTaskTracker->cancelPendingTasks();
+}
+
+bool
+VesselSetSimple::isRoiSet() const
+{
+    return m_file->isRoiSet();
+}
+
+isize_t
+VesselSetSimple::getSizeGlobalVS()
+{
+    return m_file->getSizeGlobalVS();
+}
+
+void
+VesselSetSimple::setSizeGlobalVS(const isize_t inSizeGlobalVS)
+{
+    m_file->setSizeGlobalVS(inSizeGlobalVS);
+}
+
+std::vector<int16_t>
+VesselSetSimple::getMatches()
+{
+    return m_file->getMatches();
+}
+
+void
+VesselSetSimple::setMatches(const std::vector<int16_t> & inMatches)
+{
+    m_file->setMatches(inMatches);
+}
+
+std::vector<uint16_t>
+VesselSetSimple::getEfocusValues ()
+{
+    return m_file->getEfocusValues();
+}
+
+void
+VesselSetSimple::setEfocusValues (const std::vector<uint16_t> &inEfocus)
+{
+    m_file->setEfocusValues(inEfocus);
+};
+
+std::vector<double>
+VesselSetSimple::getPairScores()
+{
+    return m_file->getPairScores();
+}
+
+void
+VesselSetSimple::setPairScores(const std::vector<double> & inPairScores)
+{
+    m_file->setPairScores(inPairScores);
+}
+
+std::vector<double>
+VesselSetSimple::getCentroidDistances()
+{
+    return m_file->getCentroidDistances();
+}
+
+void
+VesselSetSimple::setCentroidDistances(const std::vector<double> & inCentroidDistances)
+{
+    m_file->setCentroidDistances(inCentroidDistances);
+}
+
+//bool
+//VesselSetSimple::hasMetrics() const
+//{
+//    return m_file->hasMetrics();
+//}
+//
+//SpImageMetrics_t
+//VesselSetSimple::getImageMetrics(isize_t inIndex) const
+//{
+//    return m_file->getImageMetrics(inIndex);
+//}
+//
+//void
+//VesselSetSimple::setImageMetrics(isize_t inIndex, const SpImageMetrics_t & inMetrics)
+//{
+//    m_file->setImageMetrics(inIndex, inMetrics);
+//}
+
+std::string
+VesselSetSimple::getExtraProperties() const
+{
+    return m_file->getExtraProperties();
+}
+
+void
+VesselSetSimple::setExtraProperties(const std::string & inProperties)
+{
+    m_file->setExtraProperties(inProperties);
+}
+
+SpacingInfo
+VesselSetSimple::getOriginalSpacingInfo() const
+{
+    return m_file->getOriginalSpacingInfo();
+}
+
+} // namespace isx
---
+++ src/isxVesselSetSimple.h
@@ -0,0 +1,202 @@
+#ifndef ISX_VESSEL_SET_SIMPLE_H
+#define ISX_VESSEL_SET_SIMPLE_H
+
+#include "isxVesselSet.h"
+#include "isxCoreFwd.h"
+#include "isxAsync.h"
+#include "isxMutex.h"
+
+#include <fstream>
+#include <map>
+#include <memory>
+
+namespace isx
+{
+template <typename T> class IoTaskTracker;
+
+/// Forward declare needed because VesselSetFile is in src.
+class VesselSetFile;
+
+/// A class for a containing vessels extracted from a movie
+///
+/// Currently all reads/writes happen on calling threads and
+/// not on the IO thread.
+class VesselSetSimple
+    : public VesselSet
+    , public std::enable_shared_from_this<VesselSetSimple>
+{
+public:
+
+    /// Empty constructor.
+    ///
+    /// This creates an invalid vessel set and is for allocation purposes only.
+    VesselSetSimple();
+
+    /// Read constructor.
+    ///
+    /// This opens an existing vessel set file and reads information from its
+    /// header.
+    ///
+    /// \param  inFileName  The name of the vessel set file to read.
+    /// \param  enableWrite     Set to true to open in read-write mode
+    /// \throw  isx::ExceptionFileIO    If reading the vessel set file fails.
+    /// \throw  isx::ExceptionDataIO    If parsing the vessel set file fails.
+    VesselSetSimple(const std::string & inFileName, bool enableWrite = false);
+
+    /// Write constructor.
+    ///
+    /// This opens a new file, writes header information.
+    ///
+    /// \param  inFileName      The name of the vessel set file to write.
+    /// \param  inTimingInfo    The timing information of the vessel set.
+    /// \param  inSpacingInfo   The spacing information of the vessel set.
+    /// \param  inIsRoiSet      True if this came from drawing ROIs, false otherwise.
+    /// \throw  isx::ExceptionFileIO    If writing the vessel set file fails.
+    /// \throw  isx::ExceptionDataIO    If formatting the vessel set data fails.
+    VesselSetSimple(const std::string & inFileName,
+            const TimingInfo & inTimingInfo,
+            const SpacingInfo & inSpacingInfo,
+            const bool inIsRoiSet = false);
+
+    /// Destructor.
+    ///
+    ~VesselSetSimple();
+
+    // Overrides
+    bool
+    isValid() const override;
+
+    void
+    closeForWriting() override;
+
+    std::string
+    getFileName() const override;
+
+    const isize_t
+    getNumVessels() override;
+
+    isx::TimingInfo
+    getTimingInfo() const override;
+
+    isx::TimingInfos_t
+    getTimingInfosForSeries() const override;
+
+    isx::SpacingInfo
+    getSpacingInfo() const override;
+
+    SpFTrace_t
+    getTrace(isize_t inIndex) override;
+
+    void
+    getTraceAsync(isize_t inIndex, VesselSetGetTraceCB_t inCallback) override;
+
+    SpImage_t
+    getImage(isize_t inIndex) override;
+
+    void
+    getImageAsync(isize_t inIndex, VesselSetGetImageCB_t inCallback) override;
+
+    void
+    writeImageAndTrace(
+            isize_t inIndex,
+            const SpImage_t & inImage,
+            SpFTrace_t & inTrace,
+            const std::string & inName = std::string()) override;
+
+    VesselSet::VesselStatus
+    getVesselStatus(isize_t inIndex) override;
+
+    Color
+    getVesselColor(isize_t inIndex) override;
+
+    std::string
+    getVesselStatusString(isize_t inIndex) override;
+
+    void
+    setVesselStatus(isize_t inIndex, VesselSet::VesselStatus inStatus) override;
+
+    void
+    setVesselColor(isize_t inIndex, const Color& inColor) override;
+
+    void
+    setVesselColors(const IdColorPairs &inColors) override;
+
+    std::string
+    getVesselName(isize_t inIndex) override;
+
+    void
+    setVesselName(isize_t inIndex, const std::string & inName) override;
+
+    std::vector<bool>
+    getVesselActivity(isize_t inIndex) const override;
+
+    void
+    setVesselActive(isize_t inIndex, const std::vector<bool> & inActive) override;
+
+    void
+    cancelPendingReads() override;
+
+    bool
+    isRoiSet() const override;
+
+    isize_t
+    getSizeGlobalVS() override;
+
+    void
+    setSizeGlobalVS(const isize_t inSizeGlobalVS) override;
+
+    std::vector<int16_t>
+    getMatches() override;
+
+    void
+    setMatches(const std::vector<int16_t> & inMatches) override;
+
+    std::vector<uint16_t>
+    getEfocusValues() override;
+
+    void
+    setEfocusValues(const std::vector<uint16_t> & inEfocus) override;
+
+    std::vector<double>
+    getPairScores() override;
+
+    void
+    setPairScores(const std::vector<double> & inPairScores) override;
+
+    std::vector<double>
+    getCentroidDistances() override;
+
+    void
+    setCentroidDistances(const std::vector<double> & inCentroidDistances) override;
+
+//    bool
+//    hasMetrics() const override;
+//
+//    SpImageMetrics_t
+//    getImageMetrics(isize_t inIndex) const override;
+//
+//    void
+//    setImageMetrics(isize_t inIndex, const SpImageMetrics_t & inMetrics) override;
+
+    std::string
+    getExtraProperties() const override;
+
+    void
+    setExtraProperties(const std::string & inProperties) override;
+
+    SpacingInfo
+    getOriginalSpacingInfo() const override;
+
+private:
+
+    /// True if the vessel set is valid, false otherwise.
+    bool m_valid = false;
+
+    /// The vessel set file to read/write from/to.
+    std::shared_ptr<VesselSetFile>              m_file;
+    std::shared_ptr<IoTaskTracker<FTrace_t>>    m_traceIoTaskTracker;
+    std::shared_ptr<IoTaskTracker<Image>>       m_imageIoTaskTracker;
+};
+
+}
+#endif // ISX_VESSEL_SET_SIMPLE_H
